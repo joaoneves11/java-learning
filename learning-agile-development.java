93fa27079aa2c43fea70fcff7419069c001a4232
João Neves <joaopedro@outlook.com.br>
2024-01-29 14:44:25 -0300

    principios do desenvolvimento ágil

---
+++ learning-agile-development.java
@@ -0,0 +1,15 @@
+Principios de Desenvolvimento ágil
+
+Ser ágil é se adaptar/ responder rápido
+Conseguir mudar de direção rápido
+
+Um processo de desenvolvimento não pode ser o foco principal
+O foco tem que ser nas pessoas e como elas interagem
+Deve-se aproveitar ao máximo o potencial das pessoas, as pessoas não precisam ser substituíveis
+
+
+Com o feedback do cliente ir evoluindo o software em pequenas iterações
+TDD e refatoração deixam o código mais fácil de ser modificado.
+
+Documentação será gerada durante o projeto, mas ela só será utilizada quando agregar valor.
+
